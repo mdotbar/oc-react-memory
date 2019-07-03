33dddeb7290ef6d399d2f090b55ff38912bfe483
Maxime Bar <kxm9qz38lq@gmail.com>
2019-07-03 21:59:13 +0200

    Updates

---
+++ precache-manifest.99ac0ed3b2d1b29415563c8b683bb95c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801b39ee0eaa39593a0be0ad56979645",
+    "url": "/oc-react-memory/index.html"
+  },
+  {
+    "revision": "fd389bb18d4b0b558c47",
+    "url": "/oc-react-memory/static/css/main.ebd4f043.chunk.css"
+  },
+  {
+    "revision": "e8790f1586915de55759",
+    "url": "/oc-react-memory/static/js/2.92409e51.chunk.js"
+  },
+  {
+    "revision": "fd389bb18d4b0b558c47",
+    "url": "/oc-react-memory/static/js/main.3818b1c6.chunk.js"
+  },
+  {
+    "revision": "b24e011c60f9174d5f49",
+    "url": "/oc-react-memory/static/js/runtime~main.3b890ed2.js"
+  }
+]);
